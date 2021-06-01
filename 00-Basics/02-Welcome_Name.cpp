de6ebc75b7913186ada44166f79c8fbfe47980e4
Ayush Thasale <ayushthasale21@gmail.com>
2021-06-01 12:34:31 +0530

    add welcome.cpp

---
+++ 00-Basics/02-Welcome_Name.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Data Type String
+    string name;
+    cout << "Enter Your Name" << endl;
+    cin >> name;
+    cout << "Name Is " << name << endl;
+    return 0;
+}
