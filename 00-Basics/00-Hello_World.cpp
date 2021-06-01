2097d6fc1a9728e102f335c5ac462fbe7d4a904e
Ayush Thasale <ayushthasale21@gmail.com>
2021-06-01 12:18:08 +0530

    add hello_world.cpp

---
+++ 00-Basics/00-Hello_World.cpp
@@ -0,0 +1,14 @@
+// Includes Header File Input-Output Stream
+#include <iostream>
+
+// Using Namespace To Avoid Writing std Again
+using namespace std;
+
+int main()
+{
+    // Console Out
+    // << : Insertion Operator
+    cout << "Hello World\n";
+
+    return 0;
+}
