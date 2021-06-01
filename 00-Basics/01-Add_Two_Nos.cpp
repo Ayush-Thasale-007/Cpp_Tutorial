135746d507ff5bad95bdac7bf1fd2915256b5b0c
Ayush Thasale <ayushthasale21@gmail.com>
2021-06-01 12:30:37 +0530

    add Add_2_Nos file

---
+++ 00-Basics/01-Add_Two_Nos.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout << "Enter Two Nos" << endl;
+    // Reads The Value Console Input
+    // Dont Use Comma Use >>
+    cin >> a >> b;
+    c = a + b;
+    std::cout << "Answer is " << c << std::endl;
+
+    return 0;
+}
