b285b478738c3e90c4e74939cbde434a7b2712a5
Pronomita Dey <dey.pronomita@gmail.com>
2021-10-02 09:18:00 +0530

    Update Exer_03.cpp

--- 05-Strings/Exer_03.cpp
+++ 05-Strings/Exer_03.cpp
@@ -7,10 +7,15 @@ using namespace std;
 int main()
 {
     string str = "johndoe@gmail.com";
+    // find() function returns the index of the 1st occurance of the character('@') passed in our given string.
+    // ref: https://www.geeksforgeeks.org/std-find-in-cpp/
     int index = str.find('@');
     string username = "";
+    // resize function will trim the string from the supplied index.
+    // ref: https://www.geeksforgeeks.org/stdstringresize-in-c/
     username.resize(index);
     username = str.substr(0, index);
+    
     // for (int i = 0; i < index; i++)
     // {
     //     username[i] = str[i];
